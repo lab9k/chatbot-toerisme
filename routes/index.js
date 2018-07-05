45c96b716d85147a4545073a4a516c84f26ccd64
JorgWieme <Jorg.wieme@ugent.be>
2018-07-05 16:23:03 +0200

    Project structure

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+/**
+ * Routes HTTP POST requests to index
+ */
+router.post("/", function (req, res) {
+
+});
+
+module.exports = router;
