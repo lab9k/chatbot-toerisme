b98b2b38952d7b47a48b56a3bf8cfa43c7ed5339
Tom Lauwaerts <tom.lauwaerts@ugent.be>
2018-07-06 15:51:29 +0200

    Add resource file for welcomeHandler

--- handlers/buurtHandler.js
+++ handlers/buurtHandler.js
@@ -1,3 +1,8 @@
+/**
+ * Handler for "Plaatsen in de buurt" intent
+ *
+ * @param agent
+ */
 function buurtHandler(agent) {
     agent.add(`Dingen om te doen in jouw buurt: Gent bezoeken.`);
 }
--- handlers/welcomeHandler.js
+++ handlers/welcomeHandler.js
@@ -1,9 +1,15 @@
-const { Suggestion } = require('dialogflow-fulfillment');
+const { Suggestion } = require("dialogflow-fulfillment");
+const resource = require("../res/welcome");
 
+/**
+ * Handler for Default welcome intent
+ *
+ * @param agent
+ */
 function welcomeHandler(agent) {
-    agent.add(`Hallo!`);
-    agent.add(new Suggestion(`Plaatsen in de buurt`));
-    agent.add(new Suggestion(`Plaatsen per categorie`));
+    agent.add(resource.hello[agent.locale]);
+    agent.add(resource.vicinity[agent.locale]);
+    agent.add(resource.category[agent.locale]);
 }
 
 module.exports = welcomeHandler;
