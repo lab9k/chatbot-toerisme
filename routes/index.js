ff756bc4e5896ebb411dfd4c9920ae3c3c97c6a3
Tom Lauwaerts <tom.lauwaerts@ugent.be>
2018-07-06 14:13:51 +0200

    Fix to many arguments for response.send()

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,11 @@ router.post("/", function (request, response) {
     // TODO: validate origin/host
     // response.send("Invalid domain", 403);
     if (!request.hasOwnProperty("body") || Object.keys(request.body).length === 0) {
-        response.send("Empty body", 400);
+        response.status(400).send("Empty body");
     }
     if (!request.body.hasOwnProperty("queryResult") || Object.keys(request.body.queryResult).length === 0 ||
         !request.body.queryResult.hasOwnProperty("queryText") || Object.keys(request.body.queryResult.queryText).length === 0) {
-        response.send("Invalid data format", 400);
+        response.status(400).send("Invalid data format");
     }
 
     const agent = new WebhookClient({request, response});
