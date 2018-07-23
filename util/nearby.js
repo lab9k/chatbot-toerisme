5e1e3afedaf232a529195d2e1983646f88087463
Jefwillems <jefwillems100@gmail.com>
2018-07-23 17:06:48 +0200

    use const instead of var

--- util/nearby.js
+++ util/nearby.js
@@ -11,18 +11,18 @@ function toRad($Value) {
 const distance = location => toLocation => {
   const [currentLong, currentLat] = location.geometry.coordinates;
   const { long: toLong, lat: toLat } = toLocation;
-  var R = 6371e3; // metres
-  var φ1 = toRad(currentLat);
-  var φ2 = toRad(toLat);
-  var Δφ = toRad(toLat - currentLat);
-  var Δλ = toRad(toLong - currentLong);
+  const R = 6371e3; // metres
+  const φ1 = toRad(currentLat);
+  const φ2 = toRad(toLat);
+  const Δφ = toRad(toLat - currentLat);
+  const Δλ = toRad(toLong - currentLong);
 
-  var a =
+  const a =
     Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
     Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-  var d = R * c;
+  const d = R * c;
   console.log(`${location.properties.NAAM}: \n\t${d} meter\n`);
   return d;
 };
