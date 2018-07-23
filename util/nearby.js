2fd1539e02c7b1b54dfdd15d49193ac419f45b45
Jefwillems <jefwillems100@gmail.com>
2018-07-23 17:42:30 +0200

    now uses url's of .kml files, so more different data can be processed

--- util/nearby.js
+++ util/nearby.js
@@ -1,8 +1,10 @@
 const tj = require('@mapbox/togeojson');
 const fs = require('fs');
 const DOMParser = require('xmldom').DOMParser;
+const fetch = require('node-fetch');
 
-const FILE_PATH = './data/POI-Toerisme.kml';
+const FILE_PATH =
+  'http://geo.gent.be/geoserver/SG-E-CultuurSportVrijetijd/ows?service=WFS&version=1.0.0&request=GetFeature&outputFormat=application/vnd.google-earth.kml+xml&typeName=SG-E-CultuurSportVrijetijd:POI-Toerisme';
 
 function toRad($Value) {
   return ($Value * Math.PI) / 180;
@@ -27,30 +29,37 @@ const distance = location => toLocation => {
   return d;
 };
 
-const convert = filePath => {
-  const kml = new DOMParser().parseFromString(
-    fs.readFileSync(filePath, 'utf8')
-  );
-  const converted = tj.kml(kml);
-  const data = [...converted.features];
-  return function(currentUserLocation) {
-    const closest = data.reduce((previous, current) => {
-      if (
-        distance(current)(currentUserLocation) >
-        distance(previous)(currentUserLocation)
-      ) {
-        return previous;
-      } else {
-        return current;
-      }
-    });
-    return closest;
-  };
+const nearest = filePath => {
+  return fetch(filePath)
+    .then(res => {
+      return res.text();
+    })
+    .then(text => {
+      const kml = new DOMParser().parseFromString(text);
+      const converted = tj.kml(kml);
+      const data = [...converted.features];
+      return function(currentUserLocation) {
+        const closest = data.reduce((previous, current) => {
+          if (
+            distance(current)(currentUserLocation) >
+            distance(previous)(currentUserLocation)
+          ) {
+            return previous;
+          } else {
+            return current;
+          }
+        });
+        return closest;
+      };
+    })
+    .catch(error => console.log(error));
 };
 
 // * example
 // console.log(
-//   convert(FILE_PATH)({ lat: 51.055626763148624, long: 3.722346570642415 })
+//   nearest(FILE_PATH).then(cb =>
+//     cb({ lat: 51.055626763148624, long: 3.722346570642415 })
+//   )
 // );
 
-module.exports = convert(FILE_PATH);
+module.exports = nearest(FILE_PATH);
