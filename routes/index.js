3acb3ffe081e33f442a23154fb4ce506b5687341
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-07-24 13:11:22 +0200

    update middleware

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,75 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { WebhookClient } = require("dialogflow-fulfillment");
-const nearbyHandler = require("../handlers/nearbyHandler");
+const mw = require('../../util/middleware');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const nearbyHandler = require('../handlers/nearbyHandler');
+
+// Intent actions
+router.all('/', mw.typeMiddleware, (req, res, next) => {
+  let fn;
+  switch (req.type) {
+    case 'nearby_poi':
+      fn = get_nearby_poi;
+      break;
+    case 'nearest_poi':
+      fn = get_nearest_poi;
+      break;
+    default:
+      return next(
+        new Error(
+          `type not defined: ${req.type}, action: ${
+            req.body.queryResult.action
+          }`
+        )
+      );
+  }
+  return fn(req, res, next);
+});
 
 /**
  * Routes HTTP POST requests to index
  */
-router.post("/", function(request, response) {
-    // TODO: validate origin/host
-    // response.send("Invalid domain", 403);
+router.post('/', function(request, response) {
+  // TODO: validate origin/host
+  // response.send("Invalid domain", 403);
 
-    if (
-        !request.hasOwnProperty("body") ||
-        Object.keys(request.body).length === 0
-    ) {
-        response.status(400).send("Empty body");
-    }
+  if (
+    !request.hasOwnProperty('body') ||
+    Object.keys(request.body).length === 0
+  ) {
+    response.status(400).send('Empty body');
+  }
 
-    if (
-        !request.body.hasOwnProperty("queryResult") ||
-        Object.keys(request.body.queryResult).length === 0 ||
-        !request.body.queryResult.hasOwnProperty("queryText") ||
-        Object.keys(request.body.queryResult.queryText).length === 0
-    ) {
-        response.status(400).send("Invalid data format");
-    }
+  if (
+    !request.body.hasOwnProperty('queryResult') ||
+    Object.keys(request.body.queryResult).length === 0 ||
+    !request.body.queryResult.hasOwnProperty('queryText') ||
+    Object.keys(request.body.queryResult.queryText).length === 0
+  ) {
+    response.status(400).send('Invalid data format');
+  }
 
-    const agent = new WebhookClient({ request, response });
+  const agent = new WebhookClient({ request, response });
 
-    let intentMap = new Map();
-    intentMap.set("Nearby Intent", nearbyHandler);
+  let intentMap = new Map();
+  intentMap.set('Nearby Intent', nearbyHandler);
 
-    agent.handleRequest(intentMap);
+  agent.handleRequest(intentMap);
 });
 
 /**
  * Blocks all other HTTP requests
  */
-router.all("/", function(req, res) {
-    res.sendStatus(405);
+router.all('/', function(req, res) {
+  res.sendStatus(405);
 });
+
+const get_nearby_poi = (req, res) => {
+
+}
+
+const get_nearest_poi = (req, res) => {
+
+}
 
 module.exports = router;
