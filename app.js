fbcb235dc43211785ffbee2f133de5f0fe1ed21d
Jefwillems <jefwillems100@gmail.com>
2018-07-24 13:25:04 +0200

    add morgan logging

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const mw = require('./util/middleware');
 
 const index = require('./routes/index');
@@ -8,6 +9,11 @@ let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('combined'), {
+  skip: function(_req, res) {
+    return res.statusCode < 400;
+  },
+});
 app.use(mw.langMiddleWare);
 app.use(mw.typeMiddleware);
 
