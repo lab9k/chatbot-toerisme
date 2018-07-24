ad50956db9e391a38fc13b8e4488acacb24ef2cb
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-07-24 12:35:13 +0200

    update nearby poi

--- util/nearby.js
+++ util/nearby.js
@@ -1,53 +1,76 @@
-const tj = require("@mapbox/togeojson");
-const DOMParser = require("xmldom").DOMParser;
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-const FILE_PATH =
-  "http://geo.gent.be/geoserver/SG-E-CultuurSportVrijetijd/ows?service=WFS&version=1.0.0&request=GetFeature&outputFormat=application/vnd.google-earth.kml+xml&typeName=SG-E-CultuurSportVrijetijd:POI-Toerisme";
+const FILE_PATH = 'https://visit.gent.be/en/lod/poi';
 
 function toRad($Value) {
-    return ($Value * Math.PI) / 180;
+  return ($Value * Math.PI) / 180;
 }
 
 const distance = location => toLocation => {
-    const [currentLong, currentLat] = location.geometry.coordinates;
-    const { long: toLong, lat: toLat } = toLocation;
-    const R = 6371e3; // metres
-    const φ1 = toRad(currentLat);
-    const φ2 = toRad(toLat);
-    const Δφ = toRad(toLat - currentLat);
-    const Δλ = toRad(toLong - currentLong);
+  const [currentLong, currentLat] = location.geometry.coordinates;
+  const { long: toLong, lat: toLat } = toLocation;
+  const R = 6371e3; // metres
+  const φ1 = toRad(currentLat);
+  const φ2 = toRad(toLat);
+  const Δφ = toRad(toLat - currentLat);
+  const Δλ = toRad(toLong - currentLong);
 
-    const a =
+  const a =
     Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
     Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    const d = R * c;
-    return d;
+  const d = R * c;
+  return d;
 };
 
 const nearest = filePath => {
-    return fetch(filePath)
-        .then(res => {
-            return res.text();
-        })
-        .then(text => {
-            const kml = new DOMParser().parseFromString(text);
-            const converted = tj.kml(kml);
-            const data = [...converted.features];
-            return function(currentUserLocation) {
-                const closest = data.reduce((previous, current) => {
-                    if (distance(current)(currentUserLocation) > distance(previous)(currentUserLocation)) {
-                        return previous;
-                    } else {
-                        return current;
-                    }
-                });
-                return closest;
-            };
-        })
-        .catch(error => console.error(error));
+  /*return fetch(filePath)
+    .then(res => {
+      return res.text();
+    })
+    .then(text => {
+      const kml = new DOMParser().parseFromString(text);
+      const converted = tj.kml(kml);
+      const data = [...converted.features];
+      return function(currentUserLocation) {
+        const closest = data.reduce((previous, current) => {
+          if (
+            distance(current)(currentUserLocation) >
+            distance(previous)(currentUserLocation)
+          ) {
+            return previous;
+          } else {
+            return current;
+          }
+        });
+        return closest;
+      };
+    })
+    .catch(error => console.error(error));
+    */
+
+  fetch(filePath)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(JSON.stringify(data));
+      return function(currentUserLocation) {
+        const closest = data.reduce((previous, current) => {
+          if (
+            distance(current)(currentUserLocation) >
+            distance(previous)(currentUserLocation)
+          ) {
+            return previous;
+          } else {
+            return current;
+          }
+        });
+        return closest;
+      };
+    })
+    .catch(error => console.error(error));    
 };
 
 /* example
@@ -59,5 +82,13 @@ nearest(FILE_PATH)
    })
    .catch(err => console.error(err));
 */
+
+fetch(FILE_PATH)
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    console.log(JSON.stringify(data));
+  });
 
 module.exports = nearest(FILE_PATH);
