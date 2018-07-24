c6c00e9715ded99b1332e9554422783978796b7f
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-07-24 18:15:15 +0200

    log

--- handlers/nearbyHandler.js
+++ handlers/nearbyHandler.js
@@ -16,11 +16,13 @@ const getLocationCards = () => {
   return nearbyList.then(cb => {
     const loc = { lat: 51.055626763148624, long: 3.722346570642415 };
     const locations = cb(loc);
-    return locations.map(location => {
+    const cards2 = locations.map(location => {
       new Card()
         .setTitle(location.name.nl)
         .setImage(location.image[0].url);
     });
+    console.log('cards2', cards2);
+    return cards2;
   });
   
 };
