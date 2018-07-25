17552b43a4f456abe48d80ec044fc724d5cf42e2
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-25 09:59:43 +0200

    Implement categoryHandler

--- handlers/categoryHandler.js
+++ handlers/categoryHandler.js
@@ -1,8 +1,22 @@
+const poi = require('../util/poi');
+const { Suggestion } = require('dialogflow-fulfillment');
+
 /**
  * Handler for "Category" intent
  */
 function categoryHandler(agent) {
-  agent.add('Category');
+  poi.then(data => {
+    let categories = [];
+    data.forEach(poi => {
+      let category = poi.subject[0][0]['@id'];
+      if (categories.indexOf(poi.subject[0][0]['@id']) === -1) {
+        categories.push(category);
+      }
+    });
+    categories.forEach(category => {
+      agent.add(new Suggestion(category));
+    });
+  });
 }
 
 module.exports = categoryHandler;
