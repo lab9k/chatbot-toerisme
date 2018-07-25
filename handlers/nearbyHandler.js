36dfbcfcb88aa6aab81dbb58f415820dfffd07fc
Jefwillems <jefwillems100@gmail.com>
2018-07-25 10:44:01 +0200

    quickfix nearbyhandler

--- handlers/nearbyHandler.js
+++ handlers/nearbyHandler.js
@@ -18,9 +18,9 @@ const getLocationCards = () => {
   return nearbyList.then(cb => {
     const loc = { lat: 51.055626763148624, long: 3.722346570642415 };
     const locations = cb(loc);
-    const cards2 = locations.map(location => {
-      new Card().setTitle(location.name.nl[0]).setImage(location.image[0].url);
-    });
+    const cards2 = locations.map(location =>
+      new Card().setTitle(location.name.nl[0]).setImage(location.image[0].url)
+    );
     console.log('cards2', cards2);
     return cards2;
   });
