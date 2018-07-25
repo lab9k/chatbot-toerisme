b0935264a2245f65af9dc04a58dd2cbf631f5726
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-07-25 17:03:21 +0200

    remove image processing

--- util/POICard.js
+++ util/POICard.js
@@ -1,24 +1,9 @@
 const { Card } = require('dialogflow-fulfillment');
-const Jimp = require('jimp');
-const path = require('path');
-const rootDir = path.dirname(require.main.filename);
 
 class POICard extends Card {
   constructor(poi, agent) {
     super(poi.name[agent.locale][0]);
-    const nameNL = poi.name.nl[0];
-    Jimp.read(poi.image[0].url)
-      .then(function(image) {
-        image
-          .resize(512, Jimp.AUTO)
-          .quality(60)
-          .write(__dirname)
-          .write(rootDir + '/images/' + nameNL + '.jpg', Jimp.MIME_JPEG);
-      })
-      .catch(function(err) {
-        console.error(err);
-      });
-    this.setImage(rootDir + '/images/' + nameNL + '.jpg');
+    this.setImage(poi.image[0].url);
     this.setText(poi.description[agent.locale][0]);
     this.setButton({
       text: 'Toon mij de weg',
