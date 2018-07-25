c2c1b070eae1c0113484793f1dd92d3f9416f404
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-25 14:10:39 +0200

    Change image

--- handlers/nearbyHandler.js
+++ handlers/nearbyHandler.js
@@ -19,7 +19,7 @@ const getLocationCards = () => {
     const loc = { lat: 51.055626763148624, long: 3.722346570642415 };
     const locations = cb(loc);
     const cards2 = locations.map(location => {
-      return new Card(location.name.nl[0]).setImage('https://cdn.pixabay.com/photo/2016/01/14/11/43/ghent-1139784_960_720.jpg');
+      return new Card(location.name.nl[0]).setImage('https://www.photoenlarger.com/ajax.php?task=download&uid=5b586865ed853&fileName=ghent-1139784_960_720.v2.jpg');
     });
     return cards2;
   });
