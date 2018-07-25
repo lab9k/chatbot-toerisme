244c4f8c964d6186b686642c10a9fe2645444eec
Jefwillems <jefwillems100@gmail.com>
2018-07-25 10:48:23 +0200

    add more logging

--- handlers/nearbyHandler.js
+++ handlers/nearbyHandler.js
@@ -6,18 +6,23 @@ const { Card } = require('dialogflow-fulfillment');
 function nearbyHandler(agent) {
   const cardsPromise = getLocationCards();
   console.log('cardsPromise', cardsPromise);
-  cardsPromise.then(cards => {
-    cards.forEach(card => {
-      agent.add(card);
+  cardsPromise
+    .then(cards => {
+      cards.forEach(card => {
+        agent.add(card);
+      });
+      console.log('cards', cards);
+    })
+    .catch(error => {
+      console.log(error);
     });
-    console.log('cards', cards);
-  });
 }
 
 const getLocationCards = () => {
   return nearbyList.then(cb => {
     const loc = { lat: 51.055626763148624, long: 3.722346570642415 };
     const locations = cb(loc);
+    console.log(locations);
     const cards2 = locations.map(location =>
       new Card().setTitle(location.name.nl[0]).setImage(location.image[0].url)
     );
