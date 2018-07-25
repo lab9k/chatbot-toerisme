1505f08de36db6cc0ee7ace3db0e531b99b6b3cd
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-25 14:00:31 +0200

    Remove unused console logs

--- handlers/nearbyHandler.js
+++ handlers/nearbyHandler.js
@@ -5,12 +5,9 @@ const { Card } = require('dialogflow-fulfillment');
  */
 function nearbyHandler(agent) {
   const cardsPromise = getLocationCards();
-  console.log('cardsPromise', cardsPromise);
   return cardsPromise
     .then(cards => {
-      console.log('Promise resolved');
       agent.add(cards);
-      console.log('cards', cards);
     })
     .catch(error => {
       console.log(error);
@@ -22,10 +19,8 @@ const getLocationCards = () => {
     const loc = { lat: 51.055626763148624, long: 3.722346570642415 };
     const locations = cb(loc);
     const cards2 = locations.map(location => {
-      console.log(JSON.stringify(location.name));
       return new Card(location.name.nl[0]).setImage(location.image[0].url);
     });
-    console.log('cards2', cards2);
     return cards2;
   });
 };
