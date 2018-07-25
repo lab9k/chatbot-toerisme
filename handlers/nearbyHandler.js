c07615ac16692d96dd537db754931fd835fd5045
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-07-25 10:28:41 +0200

    fix nearbyHandler

--- handlers/nearbyHandler.js
+++ handlers/nearbyHandler.js
@@ -4,12 +4,14 @@ const { Card } = require('dialogflow-fulfillment');
  * Handler for "Nearby" intent
  */
 function nearbyHandler(agent) {
-  const cards = getLocationCards();
-  console.log('cards', cards);
-  cards.forEach(card => {
-    agent.add(card);
+  const cardsPromise = getLocationCards();
+  console.log('cardsPromise', cardsPromise);
+  cardsPromise.then(cards => {
+    cards.forEach(card => {
+      agent.add(card);
+    });
+    console.log('cards', cards);
   });
-  console.log(cards);
 }
 
 const getLocationCards = () => {
@@ -17,13 +19,10 @@ const getLocationCards = () => {
     const loc = { lat: 51.055626763148624, long: 3.722346570642415 };
     const locations = cb(loc);
     const cards2 = locations.map(location => {
-      new Card()
-        .setTitle(location.name.nl)
-        .setImage(location.image[0].url);
+      new Card().setTitle(location.name.nl).setImage(location.image[0].url);
     });
     console.log('cards2', cards2);
     return cards2;
   });
-  
 };
 module.exports = nearbyHandler;
