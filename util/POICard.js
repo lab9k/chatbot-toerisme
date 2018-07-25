4318b82673a3f0da34e66df126049c664b6a088d
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-25 14:24:36 +0200

    Fix POICard description

--- util/POICard.js
+++ util/POICard.js
@@ -4,7 +4,7 @@ class POICard extends Card {
   constructor(poi, agent) {
     super(poi.name[agent.locale][0]);
     this.setImage(poi.image[0].url);
-    this.setText(poi.description[agent.locale]);
+    this.setText(poi.description[agent.locale][0]);
     this.setButton({
       text: 'Toon mij de weg',
       url: `https://www.google.be/maps/dir/?api=1&destination=${poi.contactPoint.field_geofield[9]}`
