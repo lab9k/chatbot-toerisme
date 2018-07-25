8277f122b45e18df6a596adfccdfd5c6ae7a1c60
Jefwillems <jefwillems100@gmail.com>
2018-07-25 15:56:44 +0200

    add absolute path

--- util/POICard.js
+++ util/POICard.js
@@ -1,5 +1,7 @@
 const { Card } = require('dialogflow-fulfillment');
 const Jimp = require('jimp');
+const path = require('path');
+const rootDir = path.dirname(require.main.filename);
 
 class POICard extends Card {
   constructor(poi, agent) {
@@ -8,20 +10,21 @@ class POICard extends Card {
     Jimp.read(poi.image[0].url)
       .then(function(image) {
         image
-          .resize(512, Jimp.AUTO) 
-          .quality(60) 
-          .write('./images/' + nameNL+'jpg');
+          .resize(512, Jimp.AUTO)
+          .quality(60)
+          .write(__dirname)
+          .write(rootDir + '/images/' + nameNL + '.jpg');
       })
       .catch(function(err) {
         console.error(err);
       });
-    this.setImage('./images/' + nameNL+'jpg');
+    this.setImage(rootDir + '/images/' + nameNL + '.jpg');
     this.setText(poi.description[agent.locale][0]);
     this.setButton({
       text: 'Toon mij de weg',
       url: `https://www.google.be/maps/dir/?api=1&destination=${
         poi.contactPoint.field_geofield[9]
-      }`
+      }`,
     });
   }
 }
