67fd9124f7f8647eaf918b194b275bc1deb08529
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-25 14:21:39 +0200

    Use POICard

--- handlers/nearbyHandler.js
+++ handlers/nearbyHandler.js
@@ -1,10 +1,11 @@
-const nearbyList = require('../util/nearby.js');
-const { Card } = require('dialogflow-fulfillment');
+const nearby = require('../util/nearby');
+const POICard = require('../util/POICard');
+
 /**
  * Handler for "Nearby" intent
  */
 function nearbyHandler(agent) {
-  const cardsPromise = getLocationCards();
+  const cardsPromise = getLocationCards(agent);
   return cardsPromise
     .then(cards => {
       agent.add(cards);
@@ -14,14 +15,14 @@ function nearbyHandler(agent) {
     });
 }
 
-const getLocationCards = () => {
-  return nearbyList.then(cb => {
+const getLocationCards = agent => {
+  return nearby.then(cb => {
     const loc = { lat: 51.055626763148624, long: 3.722346570642415 };
     const locations = cb(loc);
-    const cards2 = locations.map(location => {
-      return new Card(location.name.nl[0]).setImage('https://www.photoenlarger.com/ajax.php?task=download&uid=5b586865ed853&fileName=ghent-1139784_960_720.v2.jpg');
+    return locations.map(location => {
+      return new POICard(location, agent);
     });
-    return cards2;
   });
 };
+
 module.exports = nearbyHandler;
--- handlers/nearestHandler.js
+++ handlers/nearestHandler.js
@@ -1,23 +1,14 @@
-const near = require('../util/nearby');
-const { Card } = require('dialogflow-fulfillment');
-const {
-  PLATFORMS,
-} = require('dialogflow-fulfillment/src/rich-responses/rich-response');
+const nearby = require('../util/nearby');
+const POICard = require('../util/POICard');
+
 /**
  * Handler for "Nearest" intent
  */
 function nearestHandler(agent) {
-  return near.then(callback => {
+  return nearby.then(callback => {
     const loc = { lat: 51.055626763148624, long: 3.722346570642415 };
-    const closest = callback(loc)[0];
-    const response = new Card(closest.name.nl[0]);
-    response.setImage(
-      'https://s3-us-west-1.amazonaws.com/greenfuture/uploads/posts/May2017/sKxM7qPlYeHJNMsFkdIP.jpg'
-    );
-    response.setPlatform(PLATFORMS.FACEBOOK);
-    response.setButton({ text: 'test', url: closest.page });
-    response.setText(closest.description);
-    agent.add(response);
+    const nearest = callback(loc)[0];
+    agent.add(new POICard(nearest, agent));
   });
 }
 
