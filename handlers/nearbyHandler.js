37284f92c98972173e9cc6d6905c21e99bb77010
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-07-25 10:33:19 +0200

    fix nearby

--- handlers/nearbyHandler.js
+++ handlers/nearbyHandler.js
@@ -19,7 +19,7 @@ const getLocationCards = () => {
     const loc = { lat: 51.055626763148624, long: 3.722346570642415 };
     const locations = cb(loc);
     const cards2 = locations.map(location => {
-      new Card().setTitle(location.name.nl).setImage(location.image[0].url);
+      new Card().setTitle(location.name.nl[0]).setImage(location.image[0].url);
     });
     console.log('cards2', cards2);
     return cards2;
