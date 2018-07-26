f532ee55e1a21943c23e5d29da3bffade9de6bb6
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-26 15:55:10 +0200

    Use local data

--- util/poi.js
+++ util/poi.js
@@ -1,11 +1,27 @@
-const fetch = require('node-fetch');
-
-const API_ENDPOINT = 'https://visit.gent.be/en/lod/poi';
+const fs = require('fs');
+const path = require('path');
 
 function fetchPOI() {
-  return fetch(API_ENDPOINT)
-    .then(res => res.json())
-    .catch(error => console.error(error));
+  return new Promise((resolve, reject) => {
+    let poi = [];
+    let dataPath = path.join(__dirname, '..', 'data');
+    fs.readdir(dataPath, (error, files) => {
+      let promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(new Promise((resolve1, reject1) => {
+          fs.readFile(path.join(dataPath, files[i]), 'utf8', (error, data) => {
+            if (error) {
+              reject1();
+            } else {
+              poi.push(...JSON.parse(data));
+              resolve1();
+            }
+          });
+        }));
+      }
+      Promise.all(promises).then(() => resolve(poi)).catch(() => reject());
+    });
+  });
 }
 
 module.exports = fetchPOI();
