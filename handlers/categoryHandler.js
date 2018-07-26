81e14147567f15380f8b1345f5dab08b523488df
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-26 10:13:30 +0200

    Add info.category intent handler

---
+++ handlers/categoryHandler.js
@@ -0,0 +1,12 @@
+const poi = require('../util/poi');
+const POICard = require('../util/POICard');
+
+/**
+ * Handler for "info.category" intent
+ */
+function categoryHandler(agent) {
+  // TODO
+  return Promise.resolve();
+}
+
+module.exports = categoryHandler;
