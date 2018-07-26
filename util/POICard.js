93f2f518de458c5b58ae8b5973997fb2387fb955
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-26 16:46:30 +0200

    Change URL to non-test URL

--- util/POICard.js
+++ util/POICard.js
@@ -3,11 +3,9 @@ const { Card } = require('dialogflow-fulfillment');
 class POICard extends Card {
   constructor(poi, locale) {
     super(poi.name[locale][0]);
-    // temp hack
-    // URL in data references image which has large size
-    // URL can be replaced to reference image which has smaller size
-    this.setImage(poi.image[poi.image.length - 1].url
-      .replace('files/', 'files/styles/header_desktop/public/'));
+    // TEMP
+    // change URL to non-test URL
+    this.setImage(poi.image[poi.image.length - 1].url.replace('web.test.gentgrp.', ''));
     this.setText(poi.description[locale][0]);
     this.setButton({
       text: 'Toon mij de weg',
