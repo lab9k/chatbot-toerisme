996b8ca407cdb9783b1c3bc21af0c5f6ad4e9530
Hervé Munyaneza <mherv2515@hotmail.com>
2018-07-26 16:07:31 +0200

    log request

--- handlers/nearbyHandler.js
+++ handlers/nearbyHandler.js
@@ -18,7 +18,7 @@ function nearbyHandler(agent) {
 
 const getLocationCards = (agent) => {
   const loc = { lat: 51.055626763148624, long: 3.722346570642415 };
-  console.log('param', util.inspect(agent.contexts));  
+  console.log('param', util.inspect(agent.originalRequest));  
   return nearby(loc)
     .then(locations => {               
       return locations.map(location => {
