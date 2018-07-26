d89194dde9526a18f7400fdcd11ab327abeeba0b
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-26 16:57:10 +0200

    Change URL to URL that references smaller image file

--- util/POICard.js
+++ util/POICard.js
@@ -4,8 +4,14 @@ class POICard extends Card {
   constructor(poi, locale) {
     super(poi.name[locale][0]);
     // TEMP
+    let url = poi.image[poi.image.length - 1].url;
     // change URL to non-test URL
-    this.setImage(poi.image[poi.image.length - 1].url.replace('web.test.gentgrp.', ''));
+    url = url.replace('web.test.gentgrp.', '');
+    // URL in data references image which has large size
+    // change URL to non-test URL
+    // URL can be replaced to reference image which has smaller size
+    url = url.replace('files/', 'files/styles/header_desktop/public/');
+    this.setImage(url);
     this.setText(poi.description[locale][0]);
     this.setButton({
       text: 'Toon mij de weg',
