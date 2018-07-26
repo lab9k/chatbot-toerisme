da7add2552eefa22ac290f8e9e757089e024edec
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-07-26 18:39:58 +0200

    fix filter

--- util/nearby.js
+++ util/nearby.js
@@ -28,7 +28,7 @@ const nearbyList = currentUserLocation => {
     .then(data =>{
       console.log('data', JSON.stringify(data).replace('\n', ' '));
       data
-      .filter(location && location.contactPoint)
+      .filter(location !== undefined && location.contactPoint)
         .map(location => ({
           dist: distance(location)(currentUserLocation),
           ...location
