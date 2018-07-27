622887f58f9c17b3749da4eba1ef289d09ee8982
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-27 10:59:51 +0200

    Use combined json

--- util/poi.js
+++ util/poi.js
@@ -3,23 +3,12 @@ const path = require('path');
 
 function fetchPOI() {
   return new Promise((resolve, reject) => {
-    let poi = [];
-    let dataPath = path.join(__dirname, '..', 'data');
-    fs.readdir(dataPath, (error, files) => {
-      let promises = [];
-      for (let i = 0; i < files.length; i++) {
-        promises.push(new Promise((resolve1, reject1) => {
-          fs.readFile(path.join(dataPath, files[i]), 'utf8', (error, data) => {
-            if (error) {
-              reject1();
-            } else {
-              poi.push(...JSON.parse(data));
-              resolve1();
-            }
-          });
-        }));
+    fs.readFile(path.join(__dirname, '..', 'data', 'combined.json'), 'utf8', (error, data) => {
+      if (error) {
+        reject();
+      } else {
+        resolve(JSON.parse(data));
       }
-      Promise.all(promises).then(() => resolve(poi)).catch(() => reject());
     });
   });
 }
