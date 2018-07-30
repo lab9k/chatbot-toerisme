fb3eadc74935560fbff57e20319ea13c8805b147
Jefwillems <jefwillems100@gmail.com>
2018-07-30 13:46:10 +0200

    add error in Promise rejection

--- util/poi.js
+++ util/poi.js
@@ -3,13 +3,17 @@ const path = require('path');
 
 function fetchPOI() {
   return new Promise((resolve, reject) => {
-    fs.readFile(path.join(__dirname, '..', 'data', 'combined.json'), 'utf8', (error, data) => {
-      if (error) {
-        reject();
-      } else {
-        resolve(JSON.parse(data));
+    fs.readFile(
+      path.join(__dirname, '..', 'data', 'combined.json'),
+      'utf8',
+      (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(JSON.parse(data));
+        }
       }
-    });
+    );
   });
 }
 
