c3a0d9da4a00062f7ca330be231b56a1cd51dc4e
Jefwillems <jefwillems100@gmail.com>
2018-08-02 16:16:08 +0200

    fix category

--- handlers/categoryHandler.js
+++ handlers/categoryHandler.js
@@ -8,11 +8,13 @@ function categoryHandler(agent) {
   return poi.then(data => {
     let cardCount = 0;
     let i = 0;
-    console.log(agent.request_.body.queryResult.queryText);
     while (i < data.length && cardCount < 10) {
       let category = data[i].subject[0][0]['@id'];
       category = category.substring(category.length - 36, category.length);
-      if (cardCount < 10 && category === agent.parameters.category) {
+      if (
+        cardCount < 10 &&
+        category === agent.request_.body.queryResult.queryText
+      ) {
         agent.add(new POICard(data[i], agent.locale));
         cardCount++;
       }
