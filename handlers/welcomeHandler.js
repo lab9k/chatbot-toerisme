ca4c5be5a25170283a31017ddfbc98e82c8c96e4
Jefwillems <jefwillems100@gmail.com>
2018-08-03 10:17:50 +0200

    add welcome handler

--- handlers/welcomeHandler.js
+++ handlers/welcomeHandler.js
@@ -1,3 +1,40 @@
-function welcomeHandler(agent) {}
+const { Payload } = require('dialogflow-fulfillment');
+const {
+  PLATFORMS,
+} = require('dialogflow-fulfillment/src/rich-responses/rich-response');
+/**
+ *
+ *
+ * @param {WebhookClient} agent
+ */
+function welcomeHandler(agent) {
+  const t = new Payload(PLATFORMS.FACEBOOK, {
+    richResponse: [
+      {
+        simpleRespone: {
+          displayText: 'Hi',
+        },
+      },
+      {
+        simpleRespone: {
+          displayText: 'Choose language',
+        },
+      },
+    ],
+    quick_replies: [
+      'English',
+      'Espanol',
+      'Nederlands',
+      'Français',
+      'Deutsch',
+    ].map(el => ({
+      content_type: 'text',
+      title: el,
+      payload: el,
+    })),
+  });
+  agent.add(t);
+  return Promise.resolve(agent);
+}
 
 module.exports = welcomeHandler;
