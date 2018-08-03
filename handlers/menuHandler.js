53579d6ec1a358a38bc33f61aaad7019cfdd61c9
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-08-03 10:45:07 +0200

    refactoring

--- handlers/menuHandler.js
+++ handlers/menuHandler.js
@@ -1,22 +1,20 @@
 const lang = require('../util/lang');
 const { Payload } = require('dialogflow-fulfillment');
 const {
-  PLATFORMS,
+  PLATFORMS
 } = require('dialogflow-fulfillment/src/rich-responses/rich-response');
+const quickReplies = require('../models/quickReplies');
 
 module.exports = function menuHandler(agent) {
-  agent.add(
-    new Payload(PLATFORMS.FACEBOOK, {
-      text: lang.translate(agent.locale, 'search_places'),
-      quick_replies: [
-        lang.translate(agent.locale, 'search_nearby'),
-        lang.translate(agent.locale, 'search_category'),
-      ].map(el => ({
-        content_type: 'text',
-        title: el,
-        payload: el,
-      })),
-    })
+  const quickReply = new quickReplies(
+    lang.translate(agent.locale, 'search_places')
   );
+  
+  quickReply.addQuickReplies(
+    lang.translate(agent.locale, 'search_nearby'),
+    lang.translate(agent.locale, 'search_category')
+  );
+
+  agent.add(new Payload(PLATFORMS.FACEBOOK, quickReply.getResponse()));
   return Promise.resolve(agent);
 };
